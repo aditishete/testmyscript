55b00d8ab3d3606fd0c4b6042726f6ad74b4cb12
aditishete <aditishete06@yahoo.com>
2023-05-25 21:18:47 -0700

    Update May25PuppeteerScript.js

--- May25PuppeteerScript.js
+++ May25PuppeteerScript.js
@@ -1,6 +1,6 @@
 const puppeteer = require('puppeteer'); // v13.0.0 or later
 
-(async () => {
+(async (require) => {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
     const timeout = 5000;
